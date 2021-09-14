17aa1b8c59ae10111ebc12b97039a2e866acab7c
Edier Sanchez <sansankind@gmail.com.com>
2021-09-14 09:02:09 -0500

    Preguntas con sus respectivas opciones y respuestas

---
+++ preguntasRespuestas.py
@@ -0,0 +1,158 @@
+preguntas = {
+
+    1: {
+        "pregunta": "The JDK command to compile a class in the file Test.java is:",
+        "respuesta": "D"
+    },
+
+    2: {
+        "pregunta": "Java was developed by...",
+        "respuesta": "C"
+    },
+
+    3: {
+        "pregunta": "...is an operating system.",
+        "respuesta": "A"
+    },
+
+    4: {
+        "pregunta": "Every statement in Java ends with:",
+        "respuesta": "C"
+    },
+
+    5: {
+        "pregunta": "...is interpreted.",
+        "respuesta": "D"
+    },
+
+    6: {
+        "pregunta": "The extension name of a Java source code file is...",
+        "respuesta": "D"
+    },
+
+    7: {
+        "pregunta": "A block is enclosed inside...",
+        "respuesta": "B"
+    },
+
+    8: {
+        "pregunta": "Math.pow(2, 3) returns ...",
+        "respuesta": "C"
+    },
+
+    9: {
+        "pregunta": "The ... method parses a string s to an int value.",
+        "respuesta": "C"
+    },
+
+    10: {
+        "pregunta": "... is the code with natural language mixed with Java code.",
+        "respuesta": "C"
+    },
+
+    11: {
+        "pregunta": "Which of these dance names is used to describe a fashionable dot?",
+        "respuesta": "D"
+    },
+
+    12: {
+        "pregunta": "What is the only position on a football team that can be 'sacked'?",
+        "respuesta": "D"
+    },
+
+    13: {
+        "pregunta": "What god of love is often depicted as a chubby winged infant with a bow and arrow?",
+        "respuesta": "C"
+    },
+
+    14: {
+        "pregunta": "Which of the following months has no U.S. ferderal holiday?",
+        "respuesta": "A"
+    },
+
+    15: {
+        "pregunta": "What mythological beast is reborn from its own ashes?",
+        "respuesta": "A"
+    },
+
+    16: {
+        "pregunta": "Who developed the first effective vaccine against polio?",
+        "respuesta": "D"
+    },
+
+    17: {
+        "pregunta": "Which of the following is not a monotheistic religion?",
+        "respuesta": "C"
+    },
+
+    18: {
+        "pregunta": "In 2014, 17-year-old Pakistani Malla Yousafzai became the youngest person ever awarded the Nobel Peace Prize in recognition of her work for what?",
+        "respuesta": "D"
+    },
+
+    19: {
+        "pregunta": "Translated from the Latin, what is the motto of the United States?",
+        "respuesta": "B"
+    },
+
+    20: {
+        "pregunta": "As part of its maintenance, which of these tourist attractions requires the use of embalming fluid?",
+        "respuesta": "A"
+    },
+
+    21: {
+        "pregunta": "Gerontology is the study of what?",
+        "respuesta": "B"
+    },
+
+    22: {
+        "pregunta": "The card game solitaire is also called what?",
+        "respuesta": "A"
+    },
+
+    23: {
+        "pregunta": "Which of the following is the title of an acclaimed PBS science series?",
+        "respuesta": "A"
+    },
+
+    24: {
+        "pregunta": "When asked why he wanted to climb Mount Everest, what explorer said, 'Because it's there'?",
+        "respuesta": "D"
+    },
+
+    25: {
+        "pregunta": "According to the well-known phrase, if a plan is not perfect what will be found 'in the ointment'?",
+        "respuesta": "C"
+    }
+}
+
+
+opciones = [
+
+  ["A. java Test.java", "B. java Test", "C. javac Test", "D. javac Test.java"],
+  ["A. IBM", "B. Microsoft", "C. Sun Microsystems", "D. Oracle"],
+  ["A. Windows 10", "B. Python", "C. C++", "D. Excel"],
+  ["A. A period (.)", "B. An asterisk (*)", "C. A semicolon (;)"],
+  ["A. Pascal", "B. Ada", "C. C++", "D. Java"],
+  ["A. .exe", "B. .class", "C. .obj", "D. .java"],
+  ["A. parentheses", "B. braces", "C. quotes", "D. brackets"],
+  ["A. 9.0", "B. 9", "C. 8.0", "D. 8"],
+  ["A. integer.parseInteger(s);" ,"B. integer.parseInt(s);", "C. Integer.parseInt(s);", "D. Integer.parseInteger(s);"],
+  ["A. A Java statement", "B. A flowchart diagram", "C. Pseudocode", "D. Java program"],
+  ["A. Hora", "B. Swing", "C. Lambada", "D. Polka"],
+  ["A. Center", "B. Wide receiver", "C. Tight end", "D. Quarterback"],
+  ["A. Zeus", "B. Mercury", "C. Cupid", "D. Poseidon"],
+  ["A. August", "B. February", "C. September", "D. November"],
+  ["A. Phoenix", "B. Minotaur", "C. Dragon", "D. Golem"],
+  ["A. Albert Sabin", "B. Niels Bohr", "C. Louis Pasteur", "D. Jonas Salk"],
+  ["A. Islam", "B. Judaism", "C. Hinduism", "D. Christianity"],
+  ["A. Animal welfare", "B. Freedom of the press", "C. Nuclear disarmament", "D. Education rights"],
+  ["A. In God we trust", "B. One out of many", "C. All as one", "D. Striving together"],
+  ["A. Lenin's tomb", "B. Mount Rushmore", "C. Stonehenge", "D. Hoover Dam"],
+  ["A. Music history", "B. Aging", "C. Color", "D. Grammar"],
+  ["A. Patience", "B. Rochambo", "C. Concentration", "D. Associations"],
+  ["A. Nova", "B. Pulsar", "C. Universe", "D. Life"],
+  ["A. Reinhold Messner", "B. Sir Edmund Hillary", "C. Peter Habeler", "D. George Mallory"],
+  ["A. Salmon", "B. Frog", "C. Fly", "D. Wildebeest"]
+
+]
